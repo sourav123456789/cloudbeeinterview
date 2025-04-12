d1a1639149a2da05eb2112be4f0fbd8f615bdb0d
sourav123456789 <souravpanigrahi78@gmail.com>
2025-04-12 12:21:14 +0530

    first commit

---
+++ src/main/java/com/example/ticketbooking/CloudbeeAssessmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.ticketbooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudbeeAssessmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudbeeAssessmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ticketbooking/CloudbeeAssessmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ticketbooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudbeeAssessmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
